6de4a6c072e794f0c36dd5b7063a881574b23248
dampolo03 <shobowaledamilolapaul@gmail.com>
2022-01-31 00:32:00 +0100

    added header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Avatar } from "../Avatar/Avatar";
+
+export const Header = () => {
+  return (
+    <header className="main-header">
+      <div>
+        <h2>Droppe</h2>
+        <h2>XMAS</h2>
+      </div>
+      <div>
+        <div className="help">Help</div>
+        <Avatar firstChar="Milly" secondChar="Spencer" />
+      </div>
+    </header>
+  );
+};
