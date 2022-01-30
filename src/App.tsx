f5eb64af4590ca5f51fa351561f0105ba7573f97
dampolo03 <shobowaledamilolapaul@gmail.com>
2022-01-31 00:25:33 +0100

    modified app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { RouterComponent } from "./router";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <RouterComponent />
+    </>
+  );
 }
 
 export default App;
